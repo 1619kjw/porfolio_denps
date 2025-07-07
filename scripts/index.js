31e2a8d5d76ebb81caf6a592d48986a0e48a4b76
jiwon <kjw89716@gmail.com>
2025-07-07 17:33:22 +0900

    6행까지html작성(DB추가필요)

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-/* 메인배너 스와이퍼 기능 */
+/* 1행 : 메인배너 스와이퍼 기능 */
 const mainSlide = new Swiper('.main',{
     // autoplay:{delay:2500,},
     pagination:{
@@ -11,3 +11,76 @@ const mainSlide = new Swiper('.main',{
         prevEl:'.swiper-wrapper ~ .swiper-button-prev',
     },
 });
+/* 2행:카테고리 가로 스크롤 스와이퍼 */
+const categorySwiper = new Swiper('.category',{
+    navigation:{
+        nextEl:'.swiper-wrapper ~ .swiper-button-next',
+        prevEl:'.swiper-wrapper ~ .swiper-button-prev',
+    },
+    slidesPerView: 'auto', // 한 화면에 슬라이드 너비만큼 자동으로 개수 표시
+    slidesPerGroup: 1,     // 한 번에 1개 이동
+    spaceBetween: 40,      // 슬라이드 간 간격
+    freeMode:false,        //하나씩 끊어서 이동됨
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+    },
+})
+/* 3행:베스트셀러 스와이퍼 */
+const bestSwiper = new Swiper('.best',{
+    navigation:{
+        nextEl:'.swiper-wrapper ~ .swiper-button-next',
+        prevEl:'.swiper-wrapper ~ .swiper-button-prev',
+    },
+    slidesPerView: 4,      // 한 화면에 4개
+    slidesPerGroup: 1,     // 한 번에 1개 이동
+    spaceBetween: 20,      // 슬라이드 간 간격
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {         // 반응형 설정 (선택사항)
+        320: { slidesPerView: 1 },
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 }
+    }
+})
+/* 4행: 신제품 스와이퍼 */
+const newSwiper = new Swiper('.new',{
+    navigation:{
+        nextEl:'.swiper-wrapper ~ .swiper-button-next',
+        prevEl:'.swiper-wrapper ~ .swiper-button-prev',
+    },
+    slidesPerView: 4,      // 한 화면에 4개
+    slidesPerGroup: 1,     // 한 번에 1개 이동
+    spaceBetween: 20,      // 슬라이드 간 간격
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {         // 반응형 설정 (선택사항)
+        320: { slidesPerView: 1 },
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 }
+    }
+})
+/* 5행 : 선물하기 상품 스크롤바 스와이퍼*/
+const giftSwiper = new Swiper('.gift', {
+    direction: "vertical",     // 세로 스크롤
+    slidesPerView: "auto",     // 콘텐츠 길이만큼 자동
+    freeMode: true,            // 자유롭게 스크롤
+    mousewheel: true,          // 마우스 휠로 스크롤 가능
+    scrollbar: {
+        el: ".gift .swiper-scrollbar",/* 부모 표시 */
+        draggable: true,
+    },
+});
+/* 6행 : 아티클 가로 스크롤바 스와이퍼 */
+const articleSwiper = new Swiper('.article',{
+    slidesPerView:3,      //한번에 3개 보이기
+    spaceBetween: 8,      // 슬라이드 간 간격
+    scrollbar: {
+        el : ".article .swiper-scrollbar",
+        draggable: true,
+    }
+})
